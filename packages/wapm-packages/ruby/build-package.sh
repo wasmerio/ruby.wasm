a28a4ebc73bd4b0138cdf11f5c278c97705997f4
Michael-F-Bryan <michael@wasmer.io>
2022-10-05 17:32:16 +0800

    Adding libruby.wasm to Ruby's WAPM package

--- packages/wapm-packages/ruby/build-package.sh
+++ packages/wapm-packages/ruby/build-package.sh
@@ -12,12 +12,15 @@ fi
 
 ruby_root="$1"
 package_dir="$(cd "$(dirname "$0")" && pwd)"
+project_root="$package_dir/../../.."
 dist_dir="$package_dir/dist"
 workdir="$(mktemp -d)"
 
 mkdir -p "$dist_dir"
 
 cp -R "$ruby_root" "$workdir/ruby-root"
+cp "$project_root/ext/witapi/bindgen/rb-abi-guest.wit" "$dist_dir"
+cp "$project_root/packages/npm-packages/ruby-wasm-wasi/dist/ruby+stdlib.wasm" "$dist_dir/libruby.wasm"
 
 (
   cd "$workdir" && \
